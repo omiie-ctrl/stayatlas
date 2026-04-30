0973db25a97360ca9154a4c89884d31a128a745f
Om Thakkar <hapmi5562@gmail.com>
2026-04-30 13:29:00 +0530

    Updated Render port

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.get("/pgs",(req,res)=>{
 res.json(pgs);
 });
 
-app.listen(3000,()=>{
-console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,() => {
+	console.log('Server running on port ${PORT}');
 });
